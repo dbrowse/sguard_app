71e38717911b3b3a4654a5d135c56af26580a866
Diyan <dgeorgiev_job@yahoo.com>
2018-05-10 15:34:11 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   # For details on th
 
-  root 'application#hello'
+
 end
